00d879fd374e98ff2f7366ecd5089c792fa0b940
Phellipe Sander <psander@id.uff.br>
2022-01-22 18:00:52 -0300

    Contact.vue

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,10 +1,44 @@
 <template>
-  <div>
-    <h1 class="font-black text-brand-main">Aqui eh a home</h1>
+  <custom-header
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+  <contact/>
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800"> feedbacker © 2022</p>
   </div>
 </template>
 <script>
+import { onMounted } from 'vue'
+import CustomHeader from './CustomHeader'
+import Contact from './Contact'
+import { useRouter } from 'vue-router'
 
+export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'FeedBacks' })
+      }
+    })
+
+    function handleLogin () {
+
+    }
+
+    function handleAccountCreate () {
+
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+}
 </script>
 <style>
 
