597d0af4750d7be7c6ba44c74e043c618c0ba10f
Phellipe Sander <psander@id.uff.br>
2022-01-22 16:45:25 -0300

    configuring project

---
+++ dashboard/src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const Home = () => import('../views/Home/index')
+const Feedbacks = () => import('../views/Feedbacks/index')
+const Credentials = () => import('../views/Credentials/index')
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/feedbacks',
+    name: 'Feedbacks',
+    component: Feedbacks,
+    meta: {
+      hasAuth: true
+    }
+  },
+  {
+    path: '/credentials',
+    name: 'Credentials',
+    component: Credentials,
+    meta: {
+      hasAuth: true
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: { name: 'Home' }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ dashboard/src/views/Home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="font-black text-brand-main">Aqui eh a home</h1>
+  </div>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
